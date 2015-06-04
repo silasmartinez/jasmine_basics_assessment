150636b10d03528182c75461857572d387f0164b
silasmartinez <silasm@gmail.com>
2015-06-04 15:18:42 -0600

    Now linted and cleaned up.

--- Grader.js
+++ Grader.js
@@ -1,62 +1,57 @@
 var math = require('mathjs')
-var letter = function(value){
-    var n = parseInt(('' + value)[(''+value).length -1]);
-    var mod = '';
+var letter = function (value) {
+  var n = parseInt(('' + value)[('' + value).length - 1], 10)
+  var mod = ''
+  if (n >= 1 && n < 4) {
+    mod = '-'
+  } else if ((n > 7 && n <= 9) || n === 0) {
+    mod = '+'
+  }
 
-    if (n >= 1 && n < 4 ) {
-        mod = '-';
-    } else if ((n > 7 && n<=9) || n == 0){
-        mod = '+';
+  if (value > 89 && value <= 105) {
+    return 'A' + mod
+  } else if (value > 79) {
+    return 'B' + mod
+  } else if (value > 69) {
+    return 'C' + mod
+  } else if (value > 59) {
+    return 'D' + mod
+  } else {
+    return 'F'
+  }
+}
+
+var average = function (arr) {
+  return arr.reduce(function (a, cur) {
+      return a + cur
+    }, 0) / arr.length
+}
+
+var mode = function (arr) {
+  var obj = arr.reduce(function (a, cur) {
+    return a.hasOwnProperty(cur) ? (a[cur] += 1, a) : (a[cur] = 1, a)
+  }, {})
+  var max = 0
+  var modes = []
+  for (var p in obj) {
+    if (obj[p] > max) {
+      max = obj[p]
     }
 
-    if (value > 89 && value <= 105){
-        return 'A'+mod;
-    } else if (value > 79){
-        return 'B'+mod;
-    } else if (value > 69){
-        return 'C'+mod;
-    } else if (value > 59){
-        return 'D'+mod;
-    } else {
-        return 'F';
+  }
+  for (p in obj) {
+    if (obj[p] === max) {
+      modes.push(parseInt(p, 10))
     }
+  }
+  modes.sort()
+  return modes
 }
-
-var average = function(arr){
-    return arr.reduce(function(a, cur){ return a + cur },0) / arr.length
-}
-
-var median = function(){
-    return 80;
-
-}
-
-var mode = function(arr){
-
-   obj = arr.reduce(function(a, cur){ return a.hasOwnProperty(cur) ? (a[cur] +=1, a) : (a[cur] = 1, a) },{})
-   var max = 0;
-   var modes = [];
-   for (var p in obj) {
-      if (obj[p] > max){
-          max = obj[p];
-      }
-   }
-    for (p in obj) {
-        if (obj[p] == max){
-            modes.push(parseInt(p));
-        }
-    }
-    modes.sort();
-   return modes;
-}
-
-
-
 
 module.exports = {
-    letterGrader: letter,
-    averageScore: average,
-    medianScore: math.median,
-    modeScore: mode,
+  letterGrader: letter,
+  averageScore: average,
+  medianScore: math.median,
+  modeScore: mode
 
 }
--- spec/spec.js
+++ spec/spec.js
@@ -1,30 +1,31 @@
 /**
  * Created by silasmartinez on 6/3/15.
  */
-var code = require('../Grader.js');
+var code = require('../Grader.js')
 
-describe('letterGrader():', function() {
-    it('test:', function() {
-        expect(code.letterGrader(98)).toEqual("A+");
-        expect(code.letterGrader(75)).toEqual("C");
-        expect(code.letterGrader(81)).toEqual("B-");
-    });
-});
+describe('letterGrader():', function () {
+  it('test:', function () {
+    expect(code.letterGrader(98)).toEqual("A+")
+    expect(code.letterGrader(75)).toEqual("C")
+    expect(code.letterGrader(81)).toEqual("B-")
+  })
+})
 
-describe('averageScore():', function() {
-    it('test:', function() {
-        expect(code.averageScore([90, 95, 87, 60])).toEqual(83);
-    });
-});
+describe('averageScore():', function () {
+  it('test:', function () {
+    expect(code.averageScore([90, 95, 87, 60])).toEqual(83)
+  })
+})
 
-describe('medianScore():', function() {
-    it('test:', function() {
-        expect(code.medianScore([52,80,80,86,94])).toEqual(80);
-    });
-});
+describe('medianScore():', function () {
+  it('test:', function () {
+    expect(code.medianScore([52, 80, 80, 86, 94])).toEqual(80)
+  })
+})
 
-describe('modeScore():', function() {
-    it('test:', function() {
-        expect(code.modeScore([82, 92, 75, 91, 92, 91, 89, 95, 100, 86])).toEqual([91,92]);
-    });
-});
+describe('modeScore():', function () {
+  it('test:', function () {
+    expect(code.modeScore([82, 92, 75, 91, 92, 91, 89, 95, 100, 86])).toEqual([91, 92])
+    expect(code.modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual([92])
+  })
+})
